6e7097fb675fb019abcb17e9df64b3a86929bc66
Alexey Shchepachyov <36983535+Solmir741@users.noreply.github.com>
2022-08-05 14:01:25 +0300

    Update infoUpdate.py

--- Back_end/infoUpdate.py
+++ Back_end/infoUpdate.py
@@ -2,18 +2,6 @@
 import requests, time, datetime, telegram_send as ts, json
 from bs4 import BeautifulSoup
 
-'''
-Done! Congratulations on your new bot. You will find it at t.me/canalservicetestBot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-Use this token to access the HTTP API:
-5503296295:AAG94Gi7HXzoa-b7EW1OjQApff557uOQQgY
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-For a description of the Bot API, see this page: 
-'''
-
-
-
 # получаем актуальный курс доллара
 def usd_update():
     response = requests.get('https://www.cbr.ru/scripts/XML_daily.asp?date_req={}'.format(datetime.date.today().strftime("%d/%m/%Y")))
